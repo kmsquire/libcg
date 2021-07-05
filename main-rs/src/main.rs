42f7e82b1d08fbaf7177af4d48085e46aeca4b46
Kevin Squire <kevin@secondspectrum.com>
2021-07-05 09:44:01 -0700

    Update main-rs
    * Format file
    * Only check result if the call succeeded

--- main-rs/src/main.rs
+++ main-rs/src/main.rs
@@ -1,38 +1,35 @@
 use libc::{c_double as double, c_int as int};
-use std::ffi::CStr;
 use std::os::raw::c_char;
 
 extern "C" {
     pub fn init_julia(argc: i32, argv: *const *const c_char);
-    //pub fn callback_fn(x: *const double, y: *const double) -> int; 
+
     pub fn julia_cg(
         fptr: unsafe extern "C" fn(*mut f64, *mut f64) -> i32,
         x: *const f64,
         y: *const f64,
-        len: u64
+        len: u64,
     ) -> i32;
     pub fn shutdown_julia(retcode: i32);
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn laplace(mut _y: *mut libc::c_double,
-                                 mut _x: *mut libc::c_double) -> libc::c_int {
+pub unsafe extern "C" fn laplace(mut _y: *mut double, mut _x: *mut double) -> int {
     let len: usize = 10;
     let c: f64 = 0.01;
 
     let y = std::slice::from_raw_parts_mut(_y, len);
     let x = std::slice::from_raw_parts_mut(_x, len);
 
-    y[0] = x[0]-c*x[1];
+    y[0] = x[0] - c * x[1];
 
-    for i in 1..(len-1){
-        y[i] = x[i] -c*(x[i-1]+x[i+1])
+    for i in 1..(len - 1) {
+        y[i] = x[i] - c * (x[i - 1] + x[i + 1])
     }
 
-    y[len-1] = x[len-1]-c*x[len-2];
+    y[len - 1] = x[len - 1] - c * x[len - 2];
     return 0 as libc::c_int;
 }
-
 
 fn main() {
     unsafe {
@@ -41,30 +38,34 @@ fn main() {
 
     let b = [1.0; 10];
     let mut x = [0.0; 10];
+    let ret: i32;
 
-    unsafe{
-        let ret = julia_cg(laplace, x.as_ptr(), b.as_ptr(), 10);
+    unsafe {
+        ret = julia_cg(laplace, x.as_ptr(), b.as_ptr(), 10);
     }
 
+    if ret != 0 {
+        println!("julia_cg call failed");
+    } else {
+        // check
+        //
+        let mut y = [0.0; 10];
+        unsafe { laplace(y.as_mut_ptr(), x.as_mut_ptr()) };
+        let mut norm2 = 0.0;
+        let mut d: f64;
+        for i in 0..10 {
+            d = b[i] - y[i];
+            norm2 += d * d;
+        }
 
-    // check
-    //
-    let mut y = [0.0; 10];
-    unsafe {laplace(y.as_mut_ptr(), x.as_mut_ptr())};
-    let mut norm2 = 0.0;
-    let mut d:f64;
-    for i in 0..10{
-        d = b[i]-y[i];
-        norm2 += d*d;
+        println!("norm: {}", norm2);
+
+        if norm2 < 1e-10 {
+            println!("success");
+        }
     }
 
-    println!("norm: {}", norm2);
-
-    if norm2 < 1e-10{
-        println!("success");
-    }
     unsafe {
         shutdown_julia(0);
     }
-
 }
